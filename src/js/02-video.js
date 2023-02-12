a80a45b36c761168296478c1f6cc00987f85d9da
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-12 20:17:14 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,32 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+let time = JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds;
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    //console.log(JSON.parse(localStorage.getItem('videoplayer-current-time')));
+  }, 1000)
+);
