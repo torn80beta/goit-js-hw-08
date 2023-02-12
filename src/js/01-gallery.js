abc8ae0c9a2af65237042c70ca3266c27af3c94b
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-12 19:18:13 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
