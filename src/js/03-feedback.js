df1b47c7414a1e4185d04808edd1550b22293d7d
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-13 21:07:33 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+mailEl = document.querySelector('input[type="email"]');
+textAreaEl = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+let data = {
+  email: mailEl.value,
+  message: textAreaEl.value,
+};
+
+mailEl.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email;
+textAreaEl.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message;
+
+mailEl.addEventListener('input', () => {
+  data.email = mailEl.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+  console.log(LOCALSTORAGE_KEY.email);
+});
+
+textAreaEl.addEventListener('input', () => {
+  data.message = textAreaEl.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+  console.log(LOCALSTORAGE_KEY.message);
+});
